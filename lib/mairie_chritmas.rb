e82080e1813fda8dd8907a2dc531c36c6dde7331
Mwah6 <sebastien.broin@gmail.com>
2019-04-11 22:36:27 +0200

    modif exo 2

--- lib/mairie_chritmas.rb
+++ lib/mairie_chritmas.rb
@@ -1,50 +1,52 @@
 require 'nokogiri'
 require 'open-uri'
 
+# Obtention de la liste des villes
+def get_city
+  city_array = []
+  Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html")).css("a.lientxt").each do |city|
+    city_array << city.text
+  end
+  print city_array
+  return city_array
 
-# def listedescommunes
-#   page2 = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
-#   list_villes = []
-#   val_doise = page2.css("a.lientxt")
-#   val_doise.each do |ville|
-#     list_villes << ville.text #remplit le tableau plus précis avec le nouveau css plus petit
-#   end
-#   puts list_villes
-# end
+end
 
-
- def get_city
-   city_array = []
-   city_array = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html")).css("a.lientxt")
- end
-
+# Obtention des urls par la liste des villes
 def get_townhall_urls (city_array)
-  city_townhall_urls = []
-  city_array.each do |ville|
-    city_townhall_urls << "http://annuaire-des-mairies.com/95/#{ville.text.downcase.gsub(" ", "-")}.html" #remplit le tableau plus précis avec le nouveau css plus petit
+  city_townhall_urls = city_array.map do |ville| #création des liens html
+    "http://annuaire-des-mairies.com/95/#{ville.downcase.gsub(" ", "-")}.html"
   end
   puts city_townhall_urls
   return city_townhall_urls
 end
 
+# Obtention des adresse email par les urls des mairies
 def get_townhall_email(townhall_url)
   townhall_email_array= []
   townhall_url.each do |url|
     townhall_email_array <<  Nokogiri::HTML(open(url)).css("td")[7].text
   end
-  # puts townhall_email_array
-  # puts townhall_email_array.size
+  print townhall_email_array
+  return townhall_email_array
 end
 
-def formulemagique city_array townhall_email_array
-final_array = []
-for n in (0..city_array.size-1) do
-  final_array << { city_array[n] => townhall_email_array[n]}
+# Mise en forme
+def formulemagique ( townhall_email_array, city_array)
+  final_array = []
+  for n in (0..city_array.size-1) do
+    final_array << { city_array[n] => townhall_email_array[n]}
+  end
+  print final_array
+  return final_array
 end
-print final_array
-return final_array
+
+# get_city
+# get_townhall_email (get_townhall_urls (get_city))
+# get_townhall_email (get_townhall_urls (get_city))
+
+def perform
+  formulemagique (get_townhall_email (get_townhall_urls (get_city))),get_city
 end
 
-
-
-formulemagique ((get_townhall_email get_townhall_urls get_city) ; get_city)
+perform
